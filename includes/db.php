124a8073402a5438131e0276ce55f2c5e3468597
Jericho <165892976+Antarticstorm@users.noreply.github.com>
2026-07-10 21:59:22 +0800

    2nd misc db change

--- includes/db.php
+++ includes/db.php
@@ -10,7 +10,7 @@ if ($_SERVER['HTTP_HOST'] == 'localhost') {
     $host = "sql207.infinityfree.com";
     $user = "if0_42378459";
     $pass = "oMpOgqNOQ0vWT";
-    $dbname = "if0_42378459_bookstore_db";
+    $db = "if0_42378459_bookstore_db";
 
 }
 
