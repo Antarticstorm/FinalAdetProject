d8eeb63245ed2b2e7ab20ef4e356a2d114f20f0d
Jericho <165892976+Antarticstorm@users.noreply.github.com>
2026-07-10 21:45:50 +0800

    fixed database problem

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "bookstore_db";
+$host = "sql207.infinityfree.com";
+$user = "if0_42378459";
+$pass = "oMpOgqNOQ0vWT";
+$dbname = "if0_42378459_bookstore_db";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
