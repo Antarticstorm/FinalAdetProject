33704a4fc5b29f8db6c1b5e94733ca572e6d97ee
Jericho <165892976+Antarticstorm@users.noreply.github.com>
2026-07-10 21:52:09 +0800

    made database dynamic
    it now reacts if you are using the infinityfree or your lcoalxampp

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,18 @@
 <?php
-$host = "sql207.infinityfree.com";
-$user = "if0_42378459";
-$pass = "oMpOgqNOQ0vWT";
-$dbname = "if0_42378459_bookstore_db";
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    // Local XAMPP
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db   = "bookstore_db";
+} else {
+    //InfinityFree
+    $host = "sql207.infinityfree.com";
+    $user = "if0_42378459";
+    $pass = "oMpOgqNOQ0vWT";
+    $dbname = "if0_42378459_bookstore_db";
+
+}
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
