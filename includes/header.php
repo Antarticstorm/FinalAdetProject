ef14083231a858eab9d9e24674cb071878069a3f
Jericho <165892976+Antarticstorm@users.noreply.github.com>
2026-07-13 23:19:38 +0800

    Reapply "Merge branch 'feature/Len' into experimental"
    This reverts commit 2e71389cedba15e506fb5e74512ec1953ce89690.

--- includes/header.php
+++ includes/header.php
@@ -43,6 +43,7 @@ in_array(
 <link rel="stylesheet" href="<?= asset('css/books.css') ?>">
 <link rel="stylesheet" href="<?= asset('css/profile.css') ?>">
 <link rel="stylesheet" href="<?= asset('css/home.css') ?>">
+<link rel="stylesheet" href="<?= asset('css/animations.css') ?>">
 <link rel="stylesheet" href="<?= asset('css/admin.css') ?>">
 <link rel="stylesheet" href="<?= asset('css/responsive.css') ?>">
 <link rel="stylesheet" href="<?= asset('css/orders.css') ?>">
